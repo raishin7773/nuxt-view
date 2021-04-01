9696a4b5a00935849496319373142d7f235046ec
raishin7773 <raishin7773@yahoo.co.jp>
2021-04-01 20:40:59 +0900

    errorページ、タイトル調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-view',
+    title: 'nuxt-view heelo',
     htmlAttrs: {
       lang: 'en'
     },
